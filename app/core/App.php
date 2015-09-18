f546faacc6de05f183bdb6a5e206c188287bd120
SanketDG <sanketdasgupta@gmail.com>
2015-09-18 22:17:44 +0530

    use Home controller
    the Home controller is now being used by App.php to correctly parse
    urls.

--- app/core/App.php
+++ app/core/App.php
@@ -13,6 +13,28 @@ class App
 
         $url = $this->parseUrl();
 
+        if(file_exists('../app/controllers/{url[0]}.php'))
+        {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/'.$this->controller.'.php';
+
+        $this->controller = new $this->controller;
+
+        if(isset($url[1]))
+        {
+            if(method_exists($this->controller, $url[1]))
+            {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        $this->param = $url ? array_values($url) : [];
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseUrl()
